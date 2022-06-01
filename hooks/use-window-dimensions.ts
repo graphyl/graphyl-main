f84523697e63f7191fb00f08e0d51f93d98cff70
LaiqueMirza <69203408+LaiqueMirza@users.noreply.github.com>
2022-06-02 00:30:13 +0530

    layout and contact

--- hooks/use-window-dimensions.ts
+++ hooks/use-window-dimensions.ts
@@ -1,26 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window
-  return {
-    width,
-    height,
-  }
-}
-
-export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  )
+export default function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: Number,
+    height: Number
+  });
 
   useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions())
-    }
-
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
-  return windowDimensions
+      function handleResize() {
+        setWindowSize({
+          // @ts-ignore
+          width: window.innerWidth,
+          // @ts-ignore
+          height: window.innerHeight,
+        });
+      }
+    
+      window.addEventListener("resize", handleResize);
+     
+      handleResize();
+    
+      return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return windowSize;
 }
