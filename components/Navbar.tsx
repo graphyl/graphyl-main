013675ab0004bc9646fc3caadd477dfa0af76a00
saad696 <shksaad911@gmail.com>
2022-06-02 10:31:45 +0530

    minor fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Button, PageHeader } from 'antd'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -6,12 +7,12 @@ const Navbar = () => {
     <>
       <PageHeader
         backIcon={false}
-        title={<h1 className='text-red-500'>Graphyl</h1>}
+        title={<h1 className="text-red-500">Graphyl</h1>}
         extra={[
           <Button key={1} type="link">
             Home
           </Button>,
-          <Button key={2} type="link">
+          <Button type="link" key={2}>
             About
           </Button>,
           <Button key={3} type="link">
