c9c7805abc225f3b7561e58ccb4fb557803d2cae
LaiqueMirza <69203408+LaiqueMirza@users.noreply.github.com>
2022-06-04 01:34:52 +0530

    carousel

---
+++ components/CarouselComp.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Carousel, Image } from 'antd'
+import styles from '../styles/CarouselComp.module.css'
+const CarouselComp: React.FC = () => {
+  return (
+      <Carousel dots={false} autoplay>
+        <div className={styles.divStyle1}>
+          <h3 className={styles.h3Style1}>WELCOME TO GRAPHYL</h3>
+        </div>
+        <div className={styles.divStyle2}>
+          <h3 className={styles.h3Style1}>
+            ITS TIME TO GO DIGITAL
+          </h3>
+        </div>
+      </Carousel>
+  )
+}
+
+export default CarouselComp
