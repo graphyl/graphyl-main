dd4d53fe7545e896bf88aa8e065690f01229d59b
LaiqueMirza <69203408+LaiqueMirza@users.noreply.github.com>
2022-06-08 19:28:32 +0530

    about

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { About } from '../components'
+
+const about = () => {
+  return (
+    <article className="text-center">
+      <About />
+    </article>
+  )
+}
+
+export default about
