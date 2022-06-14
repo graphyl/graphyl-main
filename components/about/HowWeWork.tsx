41ccf0d7a0ec412825886664051c9e3b725ca239
LaiqueMirza <69203408+LaiqueMirza@users.noreply.github.com>
2022-06-14 21:41:37 +0530

    Update HowWeWork.tsx

--- components/about/HowWeWork.tsx
+++ components/about/HowWeWork.tsx
@@ -4,34 +4,41 @@ import Link from 'next/link'
 const { Content, Sider } = Layout
 function HowWeWork() {
   return (
-    <div className="container mx-auto px-6 lg:px-0 text-center">
-
-    <Row className="mt-12 text-center">
-      <Col md={24} lg={12} className="">
-        <h2 className="text-xl">HOW WE WORK</h2>
-        <p className="mt-2 mx-16">
-          Culture Graphyl strives to create a culture that empowers a humble,
-          creative, and independent workforce. We are passionate about our
-          customers and believe that collaboration and creativity are powerful
-          tools to help them make their dreams a reality. We believe that what
-          makes us different makes us stronger.{' '}
-          <Link style={{'color':'blue'}} href="/about">Know more.</Link>
-        </p>
-      </Col>
-
-      <Col md={24} lg={12}>
-        <Image
-          loader={() =>
-            'https://www.incimages.com/uploaded_files/image/1920x1080/getty_160945425_970647970450083_44809.jpg'
-          }
-          src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_160945425_970647970450083_44809.jpg"
-          alt="work"
-          width={450}
-          height={280}
-        />
-      </Col>
-    </Row>
-</div>
+    <div className="container mx-auto my-4 px-6 lg:px-0 text-center">
+     
+      <Row
+        className="mt-12 lg:flex-row-reverse"
+        justify="center"
+        align="middle"
+        gutter={[32, 32]}
+      >
+        <Col md={24} lg={10}>
+          <Image
+            loader={() =>
+              'https://www.incimages.com/uploaded_files/image/1920x1080/getty_160945425_970647970450083_44809.jpg'
+            }
+            src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_160945425_970647970450083_44809.jpg"
+            alt="work"
+            width={450}
+            height={280}
+            className="rounded-2xl"
+          />
+        </Col>
+        <Col md={24} lg={10}>
+          <div className="text-center">
+            <h2 className="inline-block text-xl relative title-effect">
+              HOW WE WORK
+            </h2>
+          </div>
+          <p className="mt-2 mx-auto max-w-md">
+            Culture Graphyl strives to create a culture that empowers a humble,
+            creative, and independent workforce. We are passionate about our
+            customers and believe that collaboration and creativity are powerful
+            ...
+          </p>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
