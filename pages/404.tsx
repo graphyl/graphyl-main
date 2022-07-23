b1c613853b7ee2ffb0b835c589033c61b2ba976c
saad696 <shksaad911@gmail.com>
2022-07-23 15:02:09 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import notFoundImage from '../components/assets/images/404.png'
 import useWindowSize from '../hooks/use-window-dimensions'
 
 const Notfound = () => {
@@ -20,11 +19,18 @@ const Notfound = () => {
       <div className={width > 1200 ? 'p-32' : 'p-8'}>
         <Row>
           <Col xs={24} md={12}>
-            <Image src={notFoundImage} width={400} height={450} alt='404'/>
+            <Image
+              src={
+                'https://res.cloudinary.com/graphyl/image/upload/v1658562468/graphyl-website-assets/services/404_ufjg7c.png'
+              }
+              width={400}
+              height={450}
+              alt="404"
+            />
           </Col>
           <Col xs={24} md={12}>
             <div className="h-[100%] flex items-center space-y">
-              <span className='space-y-6'>
+              <span className="space-y-6">
                 <h1 className={width > 1200 ? 'text-4xl' : 'text-2xl'}>
                   Looks like you are lost!
                 </h1>
