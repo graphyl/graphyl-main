7bdfb86b13ac3b63e22aab6f41bc3eb819730e81
saad696 <shksaad911@gmail.com>
2022-07-23 14:24:33 +0530

    Update HowWeWork.tsx

--- components/about/HowWeWork.tsx
+++ components/about/HowWeWork.tsx
@@ -1,7 +1,6 @@
 // import Image from 'next/image'
 import { Layout, Image } from 'antd'
 import { aboutInfo } from '../../utils/data'
-import agile from '../assets/images/agile.png'
 const { Content, Sider } = Layout
 function HowWeWork() {
   return (
@@ -11,13 +10,13 @@ function HowWeWork() {
       </h2>
       <p className="mt-2">{aboutInfo[0].desc}</p>
       <div className="flex flex-col items-center">
-        <h3 className="font-bold text-xl title title-home" title="Work Flow">Work flow </h3>
-        <p className="mt-2 mx-auto w-[80%] text-center">
-         {aboutInfo[1].desc}
-      </p>
+        <h3 className="font-bold text-xl title title-home" title="Work Flow">
+          Work flow{' '}
+        </h3>
+        <p className="mt-2 mx-auto w-[80%] text-center">{aboutInfo[1].desc}</p>
       </div>
       <Image
-        src={agile.src}
+        src="https://res.cloudinary.com/graphyl/image/upload/v1658562468/graphyl-website-assets/services/Agile_ee7qz9.png"
         preview={false}
         alt="work"
         className="w-[70%] mx-auto"
