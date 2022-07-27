23dce0f604e46b258042dd2de9fd5d4958280a95
saad696 <shksaad911@gmail.com>
2022-07-27 14:10:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'cdn.jsdelivr.net'],
+    domains: ['res.cloudinary.com', 'https://cdn.jsdelivr.net'],
   },
 }
 
